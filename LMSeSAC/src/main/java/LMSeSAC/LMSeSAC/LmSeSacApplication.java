e324a53b547859e4b6b14a9240501ea1f1eb8191
ryeongse25 <seryeong971120@gmail.com>
2022-12-19 18:21:18 +0900

    feat: Initialize project

---
+++ LMSeSAC/src/main/java/LMSeSAC/LMSeSAC/LmSeSacApplication.java
@@ -0,0 +1,13 @@
+package LMSeSAC.LMSeSAC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmSeSacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmSeSacApplication.class, args);
+	}
+
+}
